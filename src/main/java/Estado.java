1b0fdc0b8827fc1c1ecbe8dca76e4d5c82e05f5c
AllanToledo <a.llantoledo@hotmail.com>
2023-08-16 15:15:28 -0400

    test

--- src/main/java/Estado.java
+++ src/main/java/Estado.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 @Data
 public class Estado implements Comparable<Estado> {
-    private Ponto posicao;
     @ToString.Exclude
     private List<Estado> estadosFilhos;
     @ToString.Exclude
@@ -15,30 +14,28 @@ public class Estado implements Comparable<Estado> {
     private int profudindidade = 0;
     private int euristicaEProfunidade;
     public static Map<Estado, Integer> estados = new HashMap<>();
-    public static int coeficienteProfundidade = 3;
-    public static int coeficienteHeuristica = 1;
+    public static int coeficienteProfundidade = 7;
+    public static int coeficienteHeuristica = 3;
 
-    public Estado(Ponto posicao, Estado estadoPai, Problema problema) {
-        this.posicao = posicao;
+    public Estado(Estado estadoPai, Problema problema) {
         this.estadoPai = estadoPai;
-        if (estadoPai != null)
-            this.profudindidade = estadoPai.profudindidade + 1;
         this.problema = problema;
+        this.profudindidade = estadoPai.profudindidade + 1;
         euristicaEProfunidade = this.profudindidade * coeficienteProfundidade +
                 this.problema.getHeuristica() * coeficienteHeuristica;
         estadosFilhos = new ArrayList<>();
     }
 
+    public Estado(Problema problema) {
+        this.problema = problema;
+        euristicaEProfunidade = this.problema.getHeuristica() * coeficienteHeuristica;
+        estadosFilhos = new ArrayList<>();
+    }
+
     public void gerarEstadosFilhos() {
-        if (!estadosFilhos.isEmpty()) return;
+        if(profudindidade >= 14) return;
         for (Problema problemaFilho : problema.gerarProblemasFilhos()) {
-            Ponto posicaoFilho = null;
-            if (posicao != null) {
-                float x = posicao.getX();
-                float y = posicao.getY();
-                posicaoFilho = new Ponto(x + ((float) Math.random() * 20f - 10), y + ((float) Math.random() * 20f - 10), posicao);
-            }
-            Estado novo = new Estado(posicaoFilho, this, problemaFilho);
+            Estado novo = new Estado(this, problemaFilho);
             Integer nivelRegistrado = estados.get(novo);
             if (nivelRegistrado == null || novo.profudindidade < nivelRegistrado) {
                 estados.put(novo, novo.profudindidade);
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,59 @@
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
 
-public class MainWithoutGraphics {
+public class Main {
 
     public static Queue<Estado> proximosEstados;
     public static Estado estadoRaiz;
     public static boolean keepSearching = true;
     public static int estadosExplorados = 0;
 
+    public static int estadosGerados = 0;
+
     static public void main(String[] args) {
         estadoRaiz = new Estado(
-                null,
-                null,
-                new ProblemaCuboMagico2x2(new Integer[][]{
-                        {0, 0, 4, 1, 0, 0}, //Topo
-                        {0, 0, 1, 6, 0, 0},
-                        {1, 2, 3, 5, 2, 3}, //Frente
-                        {5, 3, 4, 1, 2, 2},
-                        {0, 0, 5, 6, 0, 0}, //Baixo
-                        {0, 0, 4, 5, 0, 0},
-                        {0, 0, 6, 3, 0, 0}, //Atras
-                        {0, 0, 6, 4, 0, 0}}));
-        proximosEstados = new PriorityQueue<>();
+                new ProblemaCuboMagico2x2(new short[][]{
+                        {0, 0, 5, 5, 0, 0}, //Topo
+                        {0, 0, 1, 1, 0, 0},
+                        {2, 2, 3, 6, 2, 2}, //Frente
+                        {6, 3, 4, 4, 3, 6},
+                        {0, 0, 5, 1, 0, 0}, //Baixo
+                        {0, 0, 5, 1, 0, 0},
+                        {0, 0, 4, 4, 0, 0}, //Atras
+                        {0, 0, 3, 6, 0, 0}}));
+//        new ProblemaCuboMagico2x2(new short[][]{
+//                        {0, 0, 4, 1, 0, 0}, //Topo
+//                        {0, 0, 1, 6, 0, 0},
+//                        {1, 2, 3, 5, 2, 3}, //Frente
+//                        {5, 3, 4, 1, 2, 2},
+//                        {0, 0, 5, 6, 0, 0}, //Baixo
+//                        {0, 0, 4, 5, 0, 0},
+//                        {0, 0, 6, 3, 0, 0}, //Atras
+//                        {0, 0, 6, 4, 0, 0}}));
+        proximosEstados = new PriorityQueue<>(1 << 10);
         proximosEstados.add(estadoRaiz);
         Estado aux = estadoRaiz;
         System.out.println(aux.getProblema().getPasso());
-//        System.out.println(aux.getProblema().getHeuristica());
+        System.out.println(aux.getProblema().getHeuristica());
         System.out.println(((ProblemaCuboMagico2x2) aux.getProblema()).toStringEmoji());
+        long startTime = System.currentTimeMillis();
+        long endTime = 0;
         while (keepSearching && !proximosEstados.isEmpty()) {
             Estado estado = proximosEstados.poll();
-            assert estado != null;
             estadosExplorados++;
             if (estado.getProblema().isObjetivo()) {
-                System.out.println("Solução encontrada!");
+                endTime = System.currentTimeMillis();
+                System.out.printf("Solução encontrada! %.3fs\n", ((endTime - startTime) / 1000.0));
                 System.out.println("Número de passos para solução: " + estado.getProfudindidade());
                 System.out.println("Número de estados explorados: " + estadosExplorados);
+                System.out.println("Número de estados gerados: " + estadosGerados);
                 keepSearching = false;
                 estado.setCaminhoSolucao(true);
                 deletaEstado(estadoRaiz);
             } else {
                 estado.gerarEstadosFilhos();
+                estadosGerados += estado.getEstadosFilhos().size();
                 proximosEstados.addAll(estado.getEstadosFilhos());
             }
         }
@@ -51,10 +66,10 @@ public class MainWithoutGraphics {
             }
             System.out.println(aux.getProblema().getPasso());
 //            System.out.println(aux.getProblema().getHeuristica());
-           // System.out.println(((ProblemaCuboMagico2x2) aux.getProblema()).toStringEmoji());
+//            System.out.println(((ProblemaCuboMagico2x2) aux.getProblema()).toStringEmoji());
         }
         System.out.println(((ProblemaCuboMagico2x2) aux.getProblema()).toStringEmoji());
-
+        new Scanner(System.in).next();
     }
 
     public static void deletaEstado(Estado estado) {
